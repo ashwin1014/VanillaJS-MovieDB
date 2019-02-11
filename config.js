322e383880006df14f5a8cca8b441ffd8c3c1faf
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-02-11 16:42:33 +0530

    added movie details

--- config.js
+++ config.js
@@ -16,10 +16,11 @@ const BACKDROP_SIZE = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
 const POSTER_SIZE = 'w500';
 
+
 export {
   API_URL,
   API_KEY,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
-  POSTER_SIZE
+  POSTER_SIZE,
 };
--- script.js
+++ script.js
@@ -2,17 +2,20 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
 
 // (()=>{
      window.onload = ()=> {
+        // document.querySelector('#movieSection__details').remove();
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         fetchPopularMovieDetails(endpoint, false);
      };
 
+     
     "use strict";
     const fetchPopularMovieDetails = async (url, isSearched) => {
         let response = await fetch(url);
         let movieData = await response.json();
-    //    console.log(movieData.results[0]);
+        // console.log(movieData.results[0]);
+        isSearched ? document.querySelector('#MovieDisplayGrid h3').innerText='Search results': document.querySelector('#MovieDisplayGrid h3').innerText='Popular movies'
         movieData.results.map(element=>{
-            //  console.log(element); 
+             // console.log(element); 
             // isSearched ?  document.querySelector('.MovieDisplayGrid__container').innerHTML = '' : null
              document.querySelector('.MovieDisplayGrid__container').innerHTML += `
              <div class="col s12 m3 l3 xl3">
@@ -25,7 +28,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
                  <p class="truncate">${element.overview}</p>
                </div>
                <div class="card-action">
-                 <a href="#">Details</a>
+                 <a class="mov-details" data-reference=${element.id}>Details</a>
                </div>
              </div>
             </div>                
@@ -39,7 +42,50 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
         100%),
         url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${randomMovie.backdrop_path}'), #1c1c1c`;
         document.querySelector('.HeroImage__description h4').innerText = randomMovie.original_title;
-        document.querySelector('.HeroImage__description p').innerText = randomMovie.overview;          
+        document.querySelector('.HeroImage__description p').innerText = randomMovie.overview;   
+        
+       const movieRefernce = document.querySelectorAll('.mov-details');
+        movieRefernce.forEach((ele)=>{
+           ele.addEventListener('click', async(event)=>{
+              event.preventDefault();
+              const movieId = event.currentTarget.getAttribute('data-reference'); 
+              
+              document.querySelector('#movieSection__all').style.display="none";
+              document.querySelector('#movieSection__details').style.display="block";
+
+              let results = await Promise.all([
+                fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}`),
+                fetch(`${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`),
+              ].map(url =>
+                    url.then(
+                        (response) => response.json()
+                     ))              
+              );
+              console.log(results)
+
+              document.querySelector('#movieSection__details .container').innerHTML = `
+                  <div class="row">
+                    <div class="col m4 s12">
+                        <img class="materialboxed responsive-img" src="${IMAGE_BASE_URL}${BACKDROP_SIZE}${results[0].poster_path}">
+                    </div>
+                    <div class="col m8 s12">
+                        <h3>${results[0].original_title}</h3>
+                        <p class="">
+                        ${results[0].overview}
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="video-container">
+                        <iframe  src="https://www.youtube.com/embed/${results[1].results[0].key}" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+              `
+                var elems = document.querySelectorAll('.materialboxed');
+                var instances = M.Materialbox.init(elems);
+            });          
+
+        });
     
     };
 
@@ -53,10 +99,17 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
         }, 600);
     });
 
-    let count = 2;
+    let count = 1;
     document.getElementById('loadMoreBtn').addEventListener('click', ()=> {
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${count++}`;
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${++count}`;
         fetchPopularMovieDetails(endpoint, false);
     });
+
+    document.querySelector('.brand-logo').addEventListener('click', ()=>{
+      document.querySelector('#movieSection__details').style.display="none";
+      document.querySelector('#movieSection__all').style.display="block";
+    });
+
+
 
 // })();

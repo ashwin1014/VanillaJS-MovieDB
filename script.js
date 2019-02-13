3c503d441e0fd4e831e0219c87fbde787c56458f
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-02-13 12:50:03 +0530

    style issues on homepage

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
 
  (()=>{
      "use strict";
-    
+
      window.onload = ()=> {
         // document.querySelector('#movieSection__details').remove();
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1&include_adult=true`;
         fetchPopularMovieDetails(endpoint, false);
      };
 
-     
+
     const fetchPopularMovieDetails = async (url, isSearched) => {
         let response = await fetch(url);
         let movieData = await response.json();
@@ -18,7 +18,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
         if(isSearched) document.querySelector('.MovieDisplayGrid__container').innerHTML = '';
 
         movieData.results.map(element=>{
-             // console.log(element); 
+             // console.log(element);
              let imagePath = element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : 'images/no_image.jpg';
              document.querySelector('.MovieDisplayGrid__container').innerHTML += `
              <div class="col s6 m3 l3 xl3">
@@ -34,7 +34,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
                  <a class="mov-details" data-reference=${element.id}>Details</a>
                </div>
              </div>
-            </div>`;         
+            </div>`;
           });
 
         let randomMovie = movieData.results[Math.floor(Math.random() * 20)];
@@ -44,13 +44,13 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
         100%),
         url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${randomMovie.backdrop_path}'), #1c1c1c`;
         document.querySelector('.HeroImage__description h4').innerText = randomMovie.original_title;
-        document.querySelector('.HeroImage__description p').innerText = randomMovie.overview;   
-        
+        document.querySelector('.HeroImage__description p').innerText = randomMovie.overview;
+
        const movieRefernce = document.querySelectorAll('.mov-details');
         movieRefernce.forEach((ele)=>{
            ele.addEventListener('click', async(event)=>{
               event.preventDefault();
-              const movieId = event.currentTarget.getAttribute('data-reference'); 
+              const movieId = event.currentTarget.getAttribute('data-reference');
               let results = await Promise.all([
                 fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}&include_adult=true`),
                 fetch(`${API_URL}movie/${movieId}/videos?api_key=${API_KEY}&include_adult=true`),
@@ -58,7 +58,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
               ].map(url =>
                     url.then(
                         (response) => response.json()
-                     ))              
+                     ))
               );
                console.log(results)
               let genres = results[0].genres.map((ele)=>{
@@ -75,12 +75,12 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
               });
 
                // console.log(actors)
-              
+
 
                let actorsHtml = actors.slice(0,8).map((ele) => {
                  let imageUrl = ele.picture ? `${IMAGE_BASE_URL}/w300${ele.picture}`: 'images/no_image.jpg';
                  let html = `<div class="col s6 m3 center actor-card" data-id="${ele.actorId}">
-                 <div class="card-panel"><img class="responsive-img" src="${imageUrl}">
+                 <div class="card-panel"><img class="responsive-img hoverable" src="${imageUrl}">
                  <h6>${ele.name}</h6>
                         as
                  <h6 class="truncate">"${ele.character}"</h6>
@@ -92,7 +92,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
                 document.querySelector('i.material-icons.left').style.visibility="visible";
                 document.querySelector('#movieSection__details').style.display="block";
                 document.documentElement.scrollTop = 0;
-                
+
               let videoUrl = results[1].results[0] ? `<iframe  src="https://www.youtube.com/embed/${results[1].results[0].key}" frameborder="0" allowfullscreen></iframe>`:'<p class="flow-text">No video available</p>'
               document.querySelector('#movieSection__details .container').innerHTML = `
                   <div class="row">
@@ -145,10 +145,10 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
                     let actorData = await response.json();
                   });
                 });
-            });          
+            });
 
         });
-    
+
     };
 
     let timeout = null;
@@ -182,7 +182,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from './
         fetchPopularMovieDetails(endpoint, false);
     });
 
-    document.querySelector('.brand-logo').addEventListener('click', ()=>{      
+    document.querySelector('.brand-logo').addEventListener('click', ()=>{
       window.location.reload(true);
     });
 
